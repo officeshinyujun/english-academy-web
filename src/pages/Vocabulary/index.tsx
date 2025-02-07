9600ff287d75d6680c843730231e2eab7dcf23c7
officeshinyujun <officeshinyujun@gmail.com>
2025-02-07 22:41:14 +0900

    add : font and based and router added

---
+++ src/pages/Vocabulary/index.tsx
@@ -0,0 +1,7 @@
+export default function Vocabulary() {
+    return (
+        <div>
+            Vocabulary
+        </div>
+    )
+}
---
+++ src/pages/VoiceExtraction/index.tsx
@@ -0,0 +1,7 @@
+export default function VoiceExtraction() {
+    return (
+        <div>
+            voiceExtraction
+        </div>
+    )
+}
